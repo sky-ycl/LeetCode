b1aa746ac369b084713469d436af4fab46205d21
稚初 <1474492324@qq.com>
2023-05-08 13:14:31 +0000

    update src/month05/week01/day05/Day05_2095_1.java.
    Signed-off-by: 稚初 <1474492324@qq.com>

--- src/month05/week01/day05/Day05_2095_1.java
+++ src/month05/week01/day05/Day05_2095_1.java
@@ -2,6 +2,14 @@ package month05.week01.day05;
 
 import node.listNode.ListNode;
 
+/*
+给你一个链表的头节点 head 。删除 链表的 中间节点 ，并返回修改后的链表的头节点 head 。
+
+长度为 n 链表的中间节点是从头数起第 ⌊n / 2⌋ 个节点（下标从 0 开始），其中 ⌊x⌋ 表示小于或等于 x 的最大整数。
+
+对于 n = 1、2、3、4 和 5 的情况，中间节点的下标分别是 0、1、1、2 和 2 。
+
+*/
 public class Day05_2095_1 {
     public ListNode deleteMiddle(ListNode head) {
         if(head.next==null){
