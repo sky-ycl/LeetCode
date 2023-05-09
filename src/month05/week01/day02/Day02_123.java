a4eb2f450af882bc96877a366967c6a3607c88c5
ycl <1474492324@qq.com>
2023-05-09 15:53:23 +0800

    first commit

--- src/month05/week01/day02/Day02_123.java
+++ src/month05/week01/day02/Day02_123.java
@@ -1,5 +1,18 @@
 package month05.week01.day02;
 
+/*
+给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
+
+设计一个算法来计算你所能获取的最大利润。你最多可以完成 两笔 交易。
+
+实例：
+注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）
+输入：prices = [3,3,5,0,0,3,1,4]
+输出：6
+解释：在第 4 天（股票价格 = 0）的时候买入，在第 6 天（股票价格 = 3）的时候卖出，这笔交易所能获得利润 = 3-0 = 3 。
+   随后，在第 7 天（股票价格 = 1）的时候买入，在第 8 天 （股票价格 = 4）的时候卖出，这笔交易所能获得利润 = 4-1 = 3 。
+
+*/
 public class Day02_123 {
     public int maxProfit(int[] prices) {
         /*
@@ -10,9 +23,21 @@ public class Day02_123 {
         dp[i][4]表示第二次不持有股票最大金额
         */
         int n = prices.length;
-        int[][] dp=new int[n][5];
+        int[][] dp = new int[n][5];
         //初始化
-        dp[0][0]=0;3
-        return 0;
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        dp[0][2] = 0;
+        dp[0][3] = -prices[0];
+        dp[0][4] = 0;
+        for (int i = 1; i < n; i++) {
+            //递推公式
+            dp[i][0] = dp[i - 1][0];
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] + prices[i]);
+            dp[i][3] = Math.max(dp[i - 1][3], dp[i - 1][2] - prices[i]);
+            dp[i][4] = Math.max(dp[i - 1][4], dp[i - 1][3] + prices[i]);
+        }
+        return dp[n - 1][4];
     }
 }
